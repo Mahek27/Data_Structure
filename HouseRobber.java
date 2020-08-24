1d4bc36d03c30189338b8f6b41d81dc9bdea81ca
Mahek27 <40185448+Mahek27@users.noreply.github.com>
2020-08-24 09:40:53 -0700

    Dynamic Programming

---
+++ HouseRobber.java
@@ -0,0 +1,26 @@
+/*You are a professional robber planning to rob houses along a street. 
+ * Each house has a certain amount of money stashed, 
+ * the only constraint stopping you from robbing each of them is that adjacent houses have security system connected 
+ * and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ * Given a list of non-negative integers representing the amount of money of each house, 
+ * determine the maximum amount of money you can rob tonight without alerting the police.*/
+
+public class HouseRobber {
+	private static int[] max;
+	public static void main(String[] args) throws Exception {
+		int[] arr = {1,2,3,1};
+		int result = rob(arr);
+		System.out.print(result);
+    }
+	public static int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        max = new int[nums.length];
+        if (nums.length == 1) return nums[0];
+        max[nums.length - 1] = nums[nums.length - 1];
+        max[nums.length - 2] = Math.max(nums[nums.length - 1], nums[nums.length - 2]);
+        for (int i = nums.length - 3; i >= 0; i--) {
+            max[i] = Math.max(max[i + 1], nums[i] + max[i + 2]);
+        }
+        return max[0];
+    }
+}
