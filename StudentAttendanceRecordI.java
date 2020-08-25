8a4040a92c482269a658ab49c8d33b534e116913
Mahek27 <40185448+Mahek27@users.noreply.github.com>
2020-08-25 14:16:45 -0700

    Update StudentAttendanceRecordI.java

--- StudentAttendanceRecordI.java
+++ StudentAttendanceRecordI.java
@@ -1,4 +1,13 @@
 
+ /*	You are given a string representing an attendance record for a student. 
+ * 	The record only contains the following three characters:
+		'A' : Absent.
+		'L' : Late.
+		'P' : Present.
+ *	A student could be rewarded if his attendance record doesn't contain more 
+ *	than one 'A' (absent) or more than two continuous 'L' (late).
+ */
+
 public class StudentAttendanceRecordI {
 
 	public static void main(String[] args) {
